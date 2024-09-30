ae4d12ec01eace445c94dd6ed11807f6830b786b
farzeen5117 <farzeenmahmood22019@gmail.com>
2024-09-30 09:25:49 -0500

    added buttons on home page

--- QuizApplication/Home.Designer.cs
+++ QuizApplication/Home.Designer.cs
@@ -30,7 +30,9 @@
         {
             this.lblTeacherActions = new System.Windows.Forms.Label();
             this.btnCreateNewQuiz = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnViewExistingQuizzes = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblTeacherActions
@@ -54,16 +56,36 @@
             this.btnCreateNewQuiz.UseVisualStyleBackColor = true;
             this.btnCreateNewQuiz.Click += new System.EventHandler(this.btnCreateNewQuiz_Click);
             // 
-            // button1
+            // btnViewExistingQuizzes
             // 
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(315, 85);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(228, 110);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Create New Quiz";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnViewExistingQuizzes_Click);
+            this.btnViewExistingQuizzes.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnViewExistingQuizzes.Location = new System.Drawing.Point(306, 85);
+            this.btnViewExistingQuizzes.Name = "btnViewExistingQuizzes";
+            this.btnViewExistingQuizzes.Size = new System.Drawing.Size(228, 110);
+            this.btnViewExistingQuizzes.TabIndex = 2;
+            this.btnViewExistingQuizzes.Text = "View Existing Quizzes";
+            this.btnViewExistingQuizzes.UseVisualStyleBackColor = true;
+            this.btnViewExistingQuizzes.Click += new System.EventHandler(this.btnViewExistingQuizzes_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(560, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 110);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "View Existing Quizzes";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(306, 292);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 110);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Select Quiz To Take";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Home
             // 
@@ -71,7 +93,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightCyan;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnViewExistingQuizzes);
             this.Controls.Add(this.btnCreateNewQuiz);
             this.Controls.Add(this.lblTeacherActions);
             this.Name = "Home";
@@ -85,6 +109,8 @@
 
         private System.Windows.Forms.Label lblTeacherActions;
         private System.Windows.Forms.Button btnCreateNewQuiz;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnViewExistingQuizzes;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
