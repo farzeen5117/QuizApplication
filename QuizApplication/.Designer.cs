b221d058338b80ecaf7797daa9290bd8b0fdb4a3
KaiTrout <kaitrout07@gmail.com>
2024-09-30 09:48:43 -0500

    ,opk,i

--- QuizApplication/.Designer.cs
+++ QuizApplication/.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -49,8 +51,28 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(183, 71);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Quiz 1: Review of Number Bases and Boolean Logic";
             this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SkyBlue;
+            this.button2.Location = new System.Drawing.Point(325, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 71);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Quiz 2: Basics of CS";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.SkyBlue;
+            this.button3.Location = new System.Drawing.Point(325, 266);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 71);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Quiz 3: UML Diagrams";
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
@@ -58,10 +80,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightCyan;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form2";
-            this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -72,5 +95,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
