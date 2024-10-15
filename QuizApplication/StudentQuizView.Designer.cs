80b821f0014c8c655e894b34f54fc595db7c16f7
Ali Kasey <a.kasey13@gmail.com>
2024-10-14 20:39:04 -0500

    Opened display screen of application.

--- QuizApplication/StudentQuizView.Designer.cs
+++ QuizApplication/StudentQuizView.Designer.cs
@@ -46,7 +46,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightCyan;
-            this.ClientSize = new System.Drawing.Size(686, 411);
+            this.ClientSize = new System.Drawing.Size(899, 470);
             this.Controls.Add(this.label1);
             this.Name = "StudentQuizView";
             this.Text = "Student Quiz View";
