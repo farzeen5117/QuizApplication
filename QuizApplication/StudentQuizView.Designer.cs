7c9eacb9df60d862244a52817eda3fd4a61762ad
Ali-afk893 <A.kasey13@gmail.com>
2024-10-15 08:53:33 -0500

    we addded functionality to the submit quiz button

--- QuizApplication/StudentQuizView.Designer.cs
+++ QuizApplication/StudentQuizView.Designer.cs
@@ -64,6 +64,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Submit Quiz";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -93,7 +94,7 @@
             // 
             this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.button4.Location = new System.Drawing.Point(97, 256);
+            this.button4.Location = new System.Drawing.Point(95, 256);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(195, 29);
             this.button4.TabIndex = 4;
@@ -159,7 +160,6 @@
             this.textBox4.Text = "Question 2: Explain the difference between a compiler and an interpreter.\r\nProvid" +
     "e an example of a programming language that uses each.";
             this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // textBox5
             // 
--- QuizApplication/StudentQuizView.cs
+++ QuizApplication/StudentQuizView.cs
@@ -27,8 +27,13 @@ namespace QuizApplication
 
         }
 
-        private void textBox4_TextChanged(object sender, EventArgs e)
+        
+
+        private void button1_Click(object sender, EventArgs e)
         {
+            this.Hide();
+            Home Sp1 = new Home();
+            Sp1.Show();
 
         }
     }
